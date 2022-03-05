a2c439b9c2af988cd0499471e4230455036a5df2
Sandro Custodio <38013745+sandrocustodiobr@users.noreply.github.com>
2022-03-05 10:41:45 -0300

    Versão inicial

---
+++ bateria.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+#PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
+device=/org/freedesktop/UPower/devices/battery_BAT1	#upower -e lista os dispositivos
+percent=`upower -i ${device} | grep percentage | cut -d: -f2 | tr '%' ' ' ` 
+discharging=`upower -i ${device} | grep state | grep discharging | wc -l`
+texto='Carregando...'
+alerta=''
+tempo=`upower -i ${device} | grep 'time to empty' | cut -d: -f2`
+exibir=0
+
+#/usr/bin/notify-send "Bateria:${percent}%"
+echo "Bateria:${percent}%"
+echo "discharging:$discharging"
+
+if [ "$discharging" -eq 1 ]
+then
+	texto="Descarregando...${tempo}"
+	exibir=1
+	test "$percent -lt 30" && alerta='AVISO IMPORTANTE - '
+else
+	if [ "$percent" -lt 30 ]; then exibir=1; fi	
+fi
+
+if [ "$exibir" -eq 1 ]; then notify-send "${alerta}Bateria:${percent}%" "$texto"; fi
